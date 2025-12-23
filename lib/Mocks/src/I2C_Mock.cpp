501a04cec10fa8866499244874e5a826f47f5d38
Derv6464 <dervlag6464@gmail.com>
2025-12-23 18:22:26 +0000

    i hate diff compilers

--- lib/Mocks/src/I2C_Mock.cpp
+++ lib/Mocks/src/I2C_Mock.cpp
@@ -1,14 +1,14 @@
 #include <I2C_Mock.h>
 #include <cstddef>
 
-void I2C_Mock::write(int addr, uint8_t* data, std::size_t len) {
-    for (uint i = 0; i < len; ++i) {
+void I2C_Mock::write(int addr, uint8_t* data, size_t len) {
+    for (int i = 0; i < len; ++i) {
         write_buffer.push_back(static_cast<int>(data[i]));
     }
 }
 
-void I2C_Mock::read(int addr, uint8_t data, uint8_t* buf, std::size_t len) {
-    for (uint i = 0; i < len; ++i) {
+void I2C_Mock::read(int addr, uint8_t data, uint8_t* buf, size_t len) {
+    for (int i = 0; i < len; ++i) {
         if (i < read_buffer.size()) {
             buf[i] = static_cast<uint8_t>(read_buffer[i]);
         } else {
