67a2bc7729b09fad1a35ea5cbf5dff56a6ae291d
Derv6464 <dervlag6464@gmail.com>
2026-01-06 11:51:56 +0000

    fixing import issues

--- include/I2C/I2C_Handler.h
+++ include/I2C/I2C_Handler.h
@@ -1,11 +1,12 @@
 #ifndef I2C_HANDLER_H
 #define I2C_HANDLER_H
 #include <cstdint>
+#include <cstddef>
 
 class I2C_Handler {
     public:
-        virtual void write(int addr, uint8_t* data, size_t len) = 0;
-        virtual void read(int addr, uint8_t data, uint8_t* buf, size_t len) = 0;
+        virtual void write(int addr, uint8_t* data, std::size_t len) = 0;
+        virtual void read(int addr, uint8_t data, uint8_t* buf, std::size_t len) = 0;
         virtual ~I2C_Handler() = default;
 };
 
