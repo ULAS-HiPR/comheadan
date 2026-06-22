acd872232bdae3d4610dc54670c6c8dd026c1ecb
Derv6464 <dervlag6464@gmail.com>
2026-06-22 15:29:54 +0100

    fix: adding debug helper to can:

--- src/CAN/CAN_STM.cpp
+++ src/CAN/CAN_STM.cpp
@@ -2,6 +2,23 @@
 #include <CAN/CAN_STM.h>
 
 bool CAN_STM::init() {
+
+    CAN_FilterTypeDef filter = {0};
+
+    filter.FilterActivation = ENABLE;
+    filter.FilterBank = 0;
+    filter.FilterMode = CAN_FILTERMODE_IDMASK;
+    filter.FilterScale = CAN_FILTERSCALE_32BIT;
+    
+    filter.FilterIdHigh = 0x0000;
+    filter.FilterIdLow = 0x0000;
+    filter.FilterMaskIdHigh = 0x0000;
+    filter.FilterMaskIdLow = 0x0000;
+    
+    filter.FilterFIFOAssignment = CAN_RX_FIFO0;
+    
+    HAL_CAN_ConfigFilter(_hcan, &filter);
+
     return HAL_CAN_Start(_hcan) == HAL_OK;
 }
 
@@ -13,12 +30,23 @@ bool CAN_STM::send(CAN_Frame* frame) {
     tx_header.DLC   = frame->dlc;
 
     uint32_t tx_mailbox;
-    return HAL_CAN_AddTxMessage(
+    HAL_StatusTypeDef ret = HAL_CAN_AddTxMessage(
         _hcan,
         &tx_header,
         frame->data,
         &tx_mailbox
-    ) == HAL_OK;
+    );
+
+    uint32_t tsr = _hcan->Instance->TSR;
+    uint32_t esr = _hcan->Instance->ESR;    
+    if (ret != HAL_OK)
+    {
+        uint32_t err = HAL_CAN_GetError(_hcan);
+        return false;
+    }
+
+    return true;
+
 }
 
 bool CAN_STM::receive(CAN_Frame* frame) {
@@ -42,4 +70,5 @@ bool CAN_STM::receive(CAN_Frame* frame) {
     frame->dlc = rx_header.DLC;
     return true;
 }
+
 #endif
